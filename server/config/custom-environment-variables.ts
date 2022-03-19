689ac80158f51932003d6585ef347164e0c748cd
Julio <rios.julio.david@outlook.com>
2022-03-19 10:12:22 -0400

    config variables
    - add default and custom config variables

---
+++ server/config/custom-environment-variables.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+    apiKey: "API_KEY",
+    adminTokenPrivateKey: "ADMIN_TOKEN_PRIVATE_KEY",
+    adminTokenPublicKey: "ADMIN_TOKEN_PUBLIC_KEY",
+    accessTokenPrivateKey: "ACCESS_TOKEN_PRIVATE_KEY",
+    accessTokenPublicKey: "ACCESS_TOKEN_PUBLIC_KEY",
+    refreshTokenPrivateKey: "REFRESH_TOKEN_PRIVATE_KEY",
+    refreshTokenPublicKey: "REFRESH_TOKEN_PUBLIC_KEY",
+    dbUri: "DB_URI"
+}; 
---
+++ server/config/default.ts
@@ -0,0 +1,6 @@
+export default {
+    port: 1337,
+    dbUri: "mongodb://localhost:27017/tracking-app-db",
+    accessTokenTtl: "30m",
+    refreshTokenTtl: "1y",
+}
