da29874c9747cdf57f32c6bf09c72ede2a6e6a13
Julio <rios.julio.david@outlook.com>
2022-04-16 13:47:23 -0400

    update material controller
    implement upload material from excel file

--- server/src/resources/material/controller/material.controller.ts
+++ server/src/resources/material/controller/material.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import * as materialService from '../service/material.service';
 import HttpException from '../../../common/exceptions/http.exception';
+import fs from 'fs';
+import { findOptionsIndex } from '../utils/material.utils';
 import logger from '../../../utils/logger';
 
 export async function createMaterialHandler(
@@ -74,3 +76,66 @@ export async function deleteMaterialHandler(
     }
 }
 
+export async function uploadMaterialHandler(
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+){
+    const fileName = res.locals.fileName;
+    const worksheet: Array<any> = res.locals.worksheet;
+    const options = res.locals.options
+    let newMaterials: Array<any> = [];
+    let updatedMaterials: Array<any> = [];
+    let newMaterialCount = 0;
+    let updatedMaterialsCount = 0;
+
+
+    //length -1 to skip last line of excel file
+    // verify if the material exist, if not exist add it to DB
+    for (let i = 0; i < worksheet.length -1; i++) {
+        const optionIndex = findOptionsIndex(options, worksheet[i]["Registration number"]);
+        if(optionIndex === -1) continue;
+
+        const materialToFind = materialService.defineMaterialFromWorksheet(worksheet[i], options[optionIndex].project);
+        const materialExists = await materialService.materialExists({
+            partNumber: materialToFind.partNumber,
+            order: materialToFind.order,
+            project: materialToFind.project
+        });
+
+        if(!materialExists){
+            const savedMaterial = await materialService.createMaterial(materialToFind);
+            newMaterials.push(savedMaterial);
+            newMaterialCount++;
+            continue;
+        }
+
+        if(materialExists){
+            const queryMaterials = await materialService.queryMaterial({
+                partNumber: materialToFind.partNumber,
+                order: materialToFind.order,
+                project: materialToFind.project
+            });
+
+            if(queryMaterials && queryMaterials.length === 1 && queryMaterials[0].awb === "" && materialToFind.awb !== undefined){
+                const updatedMaterial = await materialService.updateMaterial(queryMaterials[0]._id, {awb: materialToFind.awb});
+                updatedMaterials.push(updatedMaterial);
+                updatedMaterialsCount++;
+            }
+        }         
+    }
+
+    //remove file from local storage
+    fs.unlink(fileName, (error) => {
+       logger.error(error, "[material controller] - [uploadMaterialHandler] - remove file from local storage error");
+    });
+
+
+    return res.status(201).json({
+        options,
+        newMaterialsCount: newMaterialCount,
+        updatedMaterialsCount: updatedMaterialsCount,
+        materialsAdded: newMaterials,
+        updatedMaterialsAdded: updatedMaterials,
+    });
+}
