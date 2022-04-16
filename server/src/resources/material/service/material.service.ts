676bd5e5d7fb0399cb24f82e5410a69750b0e5ea
Julio <rios.julio.david@outlook.com>
2022-04-16 13:35:57 -0400

    update material service
    implement
    - define material from worksheet
    - material exist

--- server/src/resources/material/service/material.service.ts
+++ server/src/resources/material/service/material.service.ts
@@ -1,8 +1,23 @@
 import MaterialModel, { Material } from "../model/material.model";
-import { QueryOptions } from "mongoose";
+import mongoose, { QueryOptions } from "mongoose";
 
 export async function createMaterial(input: Partial<Material>){
     return MaterialModel.create(input);
+}
+
+export function defineMaterialFromWorksheet(item: any, project: string){
+    return new MaterialModel({
+        _id: new mongoose.Types.ObjectId(),
+        project,
+        partNumber: item["Material"],
+        materialDescription: item["Material description"],
+        workCenter: item["WorkCenter"],
+        order: item["Order"],
+        orderDescription: item["Description"],
+        purchaseOrder: item["PO"],
+        awb: item["AWB"],
+        quantity: item["RequiredQty"],
+    })
 }
 
 export async function findMaterialById(id: string) {
@@ -26,4 +41,9 @@ export async function deleteMaterial(id: string){
 
 export async function deleteAllMaterials() {
     return await MaterialModel.deleteMany({});
+}
+
+export async function materialExists(query: Partial<Material>): Promise<boolean>{
+    const materialToFind = await MaterialModel.findOne(query);
+    return (materialToFind == null)? false : true; 
 }
